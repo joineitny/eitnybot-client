251de764938c5af8f34fcde204460d1687f08aeb
Anthony Al Kudai <102955248+joineitny@users.noreply.github.com>
2025-02-21 09:21:32 -0800

    0.1.1

--- main.go
+++ main.go
@@ -5,87 +5,48 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"time"
 
-	"github.com/gorilla/websocket"
-	"github.com/oschwald/geoip2-golang"
+	"golang.org/x/crypto/ssh"
 )
 
-type Client struct {
-	IP       string `json:"ip"`
-	Hostname string `json:"hostname"`
-	Location string `json:"location"`
-}
-
-func getPublicIP() string {
-	resp, err := http.Get("https://api.ipify.org?format=text")
-	if err != nil {
-		log.Fatal("Error getting public IP:", err)
-	}
-	defer resp.Body.Close()
-
-	ip := ""
-	if _, err := fmt.Fscan(resp.Body, &ip); err != nil {
-		log.Fatal("Error reading public IP:", err)
-	}
-
-	return ip
-}
-
-func getLocation(ip string) string {
-	db, err := geoip2.Open("GeoLite2-City.mmdb")
-	if err != nil {
-		log.Fatal("Error opening GeoIP database:", err)
-	}
-	defer db.Close()
-
-	record, err := db.City(net.ParseIP(ip))
-	if err != nil {
-		log.Fatal("Error getting location:", err)
-	}
-
-	return fmt.Sprintf("%s, %s", record.City.Names["en"], record.Country.Names["en"])
-}
-
-func getHostname() string {
-	hostname, err := os.Hostname()
-	if err != nil {
-		log.Fatal("Error getting hostname:", err)
-	}
-	return hostname
+type ClientInfo struct {
+	IP       string
+	Hostname string
 }
 
 func main() {
-	ip := getPublicIP()
-	hostname := getHostname()
-	location := getLocation(ip)
-
-	client := Client{
-		IP:       ip,
+	hostname, _ := os.Hostname()
+	info := ClientInfo{
+		IP:       getOutboundIP(),
 		Hostname: hostname,
-		Location: location,
 	}
 
-	conn, _, err := websocket.DefaultDialer.Dial("ws://localhost:8080/ws", nil)
+	conn, err := net.Dial("tcp", "server-ip:8080")
 	if err != nil {
-		log.Fatal("Dial error:", err)
+		log.Fatalf("Failed to connect to server: %v", err)
 	}
 	defer conn.Close()
 
+	encoder := json.NewEncoder(conn)
+	if err := encoder.Encode(info); err != nil {
+		log.Fatalf("Failed to send client info: %v", err)
+	}
+
+	// Keep the connection alive
 	for {
-		clientJSON, err := json.Marshal(client)
-		if err != nil {
-			log.Println("Marshal error:", err)
-			continue
-		}
-
-		if err := conn.WriteMessage(websocket.TextMessage, clientJSON); err != nil {
-			log.Println("Write error:", err)
-			break
-		}
-
 		time.Sleep(10 * time.Second)
 	}
+}
+
+func getOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
 }
